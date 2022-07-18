2c25020760bdc7fc766bff2936627d0b7106b1b3
DFWilliams <williams.dee.f99@gamil.com>
2022-07-18 07:19:31 -0700

    assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,42 +15,139 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
+describe("gibberishMaker", () => {
+    it("takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0", () => {
+        const secretCodeWord1 = "Lackadaisical"
+            // Expected output: [-8, 90]
+        const secretCodeWord2 = "Gobbledygook"
+            // Expected output: [5, 109]
+        const secretCodeWord3 = "Eccentric"
+      expect(gibberishMaker(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(gibberishMaker(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(gibberishMaker(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+
+
+//   ReferenceError: gibberishMaker is not defined
+
 
 
 // b) Create the function that makes the test pass.
+
+    //----Psuedo code----//
+        //declare a function called gibberishMaker
+        // set a param string
+        // declare a variable called newArray 
+        // used .split to convert the string into an array
+        //declare a variable called newNewArray
+        //use .map to iterate through each value in the array
+        // use conditional statements to determine the values that need replacement
+            // if value === "a", return value = 4
+            // else if value === "e", return value = 3
+            // else if value === "i", return value = 1
+            // else if value === "o", return value = 0
+            // else return value 
+        // use .join to convert the mapped array into a string
+
+
+
+    const gibberishMaker = (string) => {
+    //    let stringTurnedArray = string.split('')
+               let newArray = string.split('')
+               console.log((newArray))
+               let newNewArray= newArray.map ((value) => {
+            if (value.toUpperCase() === "A"){
+                return value = 4 
+            }else if (value.toUpperCase() === "E"){
+                return value = 3
+            }else if (value.toUpperCase() === "I"){
+                return value = 1
+            }else if (value.toUpperCase() === "O"){
+                return value = 0
+            }else{
+                return value 
+            }
+           })
+           return newNewArray.join('')
+        }
+
+ // Test Suites: 1 passed, 1 total
+//  This problem basically took me the entire length of The Curious Case of Benjamin Button to figure out (i took breaks), I'm going to refactor later
 
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+describe("wordsIncluding", () => {
+    it("takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const letterA = "a"
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        const letterE = "e"
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+      expect(wordsIncluding(fruitArray,letterA)).toEqual(["Mango", "Apricot", "Peach"])
+      expect(wordsIncluding(fruitArray,letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
 
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+//   ReferenceError: wordsIncluding is not defined
 
 
 // b) Create the function that makes the test pass.
+   //----Psuedo code----//
+    // declare a function called wordsIncluding
+    // set a params array and letter 
+    // use .filter to iterate through each value 
+    // use .include to return the values that contain the letter 
+        
+const wordsIncluding = (array, letter) => {
+    return array.filter (word => {
+        return word.toLowerCase().includes(letter)
+     })
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
+describe("bobSagetsHouse", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house", () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        const hand2 = [5, 5, 3, 3, 4]
+        const hand3 = [5, 5, 5, 5, 4]        
+      expect(bobSagetsHouse(hand1)).toEqual(true)
+      expect(bobSagetsHouse(hand2)).toEqual(false)
+      expect(bobSagetsHouse(hand3)).toEqual(false)
+  })
+})
 
+
+// ReferenceError: bobSagetsHouse is not defined
 
 // b) Create the function that makes the test pass.
+   //----Psuedo code----//
+    // declare a function called bobSagetHouse
+    // set param array
+    // make a for loop to interate through each value in the array
+    // use conditionals to determine if the array is a full house
+        // if [0] === [1] ===[2] && [3] === [4], return true
+        // else return false 
+
+
+    const bobSagetsHouse = (array) => {
+        for (let i=0; i < array.length; i++)
+        if ((array[0] === array[1]) && (array[1] === array[2]) && (array[3] === array[4])) {
+            return true
+        } else {
+            return false 
+        }
+    }
+    
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -3,12 +3,24 @@
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
+
+
 beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
+
+def choiceWords (array, letter)
+    array.select do |value|
+        if value.include? letter
+            value 
+        end
+    end
+end
+
+# p choiceWords beverages_array, letter_o
 
 
 # -------------------2) Create a method that takes in an array of numbers and returns the sum of the numbers. Use the test variables provided.
@@ -19,9 +31,48 @@ nums_array1 = [42, 7, 27]
 nums_array2 = [25, 17, 47, 11]
 # Expected output: 100
 
+def arraySum array
+    array.sum
+end
+
+# p arraySum nums_array1 
+# p arraySum nums_array2
+
 
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
+
+class Bike
+    attr_accessor :model, :current_speed
+    def initialize(model, wheels, current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0 
+    end
+    def bike_info
+        "The #{@model} bike has #{@model} wheels and is goin #{@current_speed}"
+    end
+    def pedal_faster num
+        @current_speed = current_speed + num
+    end
+    def brake num
+        @current_speed = current_speed - num
+        if @current_speed < 0
+            @current_speed = 0
+        end
+    end
+end
+
+p my_bike = Bike.new("Trek", 2, 0)
+p my_bike.bike_info
+p my_bike.pedal_faster(10)
+p my_bike.bike_info
+p my_bike.pedal_faster(18)
+p my_bike.brake(5)
+p my_bike.bike_info
+p my_bike.brake(25)
+p my_bike.bike_info
+
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
